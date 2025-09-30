baa6ea5b7b9df26cce7de62fd4a0d5ebb0cd4386
Sangyoon <qlrqod123123@naver.com>
2025-09-30 14:41:13 +0900

    [FIX] NavHost 이동 오류 수정

--- app/src/main/java/com/sampoom/android/app/navigation/AppNavHost.kt
+++ app/src/main/java/com/sampoom/android/app/navigation/AppNavHost.kt
@@ -5,19 +5,23 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.sampoom.android.feature.auth.ui.LoginScreen
-import com.sampoom.android.feature.part.ui.InventoryScreen
+import com.sampoom.android.feature.part.ui.PartScreen
 
 const val ROUTE_LOGIN = "login"
-const val ROUTE_INVENTORY = "inventory"
+const val ROUTE_PART = "part"
 
 @Composable
 fun AppNavHost(navController: NavHostController) {
-    NavHost(navController, startDestination = ROUTE_INVENTORY) {
+    NavHost(navController, startDestination = ROUTE_PART) {
+
+        // 로그인 화면
         composable(ROUTE_LOGIN) { LoginScreen(onSuccess = {
 //            navController.navigate("main") {
 //                popUpTo(ROUTE_LOGIN) { inclusive = true } // 로그인 화면 스택 제거
 //            }
         }) }
-        composable(ROUTE_INVENTORY) { InventoryScreen() }
+
+        // 부품 조회 화면
+        composable(ROUTE_PART) { PartScreen() }
     }
 }
